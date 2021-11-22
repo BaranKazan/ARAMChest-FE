09cfb4c4b50d9b2c2ef4243ce303a7d072a38802
Baran Kazan <baran.kazan@hotmail.com>
2021-11-22 17:03:00 +0100

    Changed the background color
    WE ARE SO CLOSE NOW!!!

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#00dde0",
+          50: "#32ffff",
+          100: "#28ffff",
+          200: "#1efbfe",
+          300: "#14f1f4",
+          400: "#0ae7ea",
+          500: "#00dde0",
+          600: "#00d3d6",
+          700: "#00c9cc",
+          800: "#00bfc2",
+          900: "#00b5b8"
+        }
+      }
+    },
   },
   variants: {
     extend: {},
