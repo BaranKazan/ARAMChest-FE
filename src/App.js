2e50730e3b1592dd2e52fc246afb09eedc3e844d
Baran Kazan <Baran.kazan@hotmail.com>
2021-11-27 21:06:53 +0100

    Added tittle

--- src/App.js
+++ src/App.js
@@ -3,13 +3,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-      <div className="flex-shrink-0">
-      </div>
-      <div>
-        <div className="text-xl font-medium text-black">ChitChat</div>
-        <p className="text-gray-500">You have a new message!</p>
-      </div>
+    <div className="w-full h-full">
+      <h1 class="font-extrabold text-transparent text-8xl bg-clip-text leading-normal bg-gradient-to-r from-green-400 to-blue-500">Hextech Discovery</h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,19 +4,6 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: {
-          DEFAULT: "#00dde0",
-          50: "#32ffff",
-          100: "#28ffff",
-          200: "#1efbfe",
-          300: "#14f1f4",
-          400: "#0ae7ea",
-          500: "#00dde0",
-          600: "#00d3d6",
-          700: "#00c9cc",
-          800: "#00bfc2",
-          900: "#00b5b8"
-        }
       }
     },
   },
