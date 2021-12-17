85cb2db385d1e53a31f28c86af5e7a277bc90674
Baran Kazan <baran.kazan@hotmail.com>
2021-12-17 15:44:27 +0100

    almost forgot

--- src/components/SummonerCard.js
+++ src/components/SummonerCard.js
@@ -3,7 +3,7 @@ import { Component } from 'react';
 class SummonerCard extends Component {
     render() {
         return (
-            <div className="flex w-full mt-4 mx-2 p-3 items-center rounded-lg shadow-lg bg-white">
+            <div className="flex w-full my-2 sm:mx-2 p-3 items-center rounded-lg shadow-lg bg-white">
                 <img className="w-10 h-10 rounded-full mr-4" src={this.props.summoner.iconURL} alt="hello" />
                 <div className="text-sm">
                     <p className="text-gray-900 leading-none">{this.props.summoner.name}</p>
