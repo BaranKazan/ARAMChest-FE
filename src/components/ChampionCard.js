f4d335c99f58963dbd00b1a95f2246bd4b1ca930
Baran Kazan <baran.kazan@hotmail.com>
2021-12-22 14:43:14 +0100

    small design changes

--- src/components/ChampionCard.js
+++ src/components/ChampionCard.js
@@ -3,9 +3,9 @@ import { Component } from 'react';
 class ChampionCard extends Component {
     render() {
         return (
-            <div className="max-w-sm bg-white rounded-2xl shadow-md text-center p-3 overflow-hidden">
-                <img className="mx-auto" src={this.props.champion.imageURL} alt="Woman's Face" />
-                <p className="font-sans text-lg text-black font-semibold">{this.props.champion.name}</p>
+            <div className="max-w-sm bg-white rounded-2xl shadow-md text-center px-3 pt-3 pb-1 overflow-hidden">
+                <img className="mx-auto rounded" src={this.props.champion.imageURL} alt="Woman's Face" />
+                <p className="text-gray-900 font-bold">{this.props.champion.name}</p>
             </div>
         );
     }
--- src/components/SummonerCard.js
+++ src/components/SummonerCard.js
@@ -4,11 +4,11 @@ class SummonerCard extends Component {
     render() {
         return (
             <div className="flex w-full my-2 sm:mx-2 p-3 items-center rounded-lg shadow-lg bg-white">
-                <img className="w-10 h-10 rounded-full mr-4" src={this.props.summoner.iconURL} alt="hello" />
+                <img className="w-16 h-16 rounded-lg mr-4" src={this.props.summoner.iconURL} alt={this.props.summoner.name} />
                 <div className="text-sm">
-                    <p className="text-gray-900 leading-none">{this.props.summoner.name}</p>
+                    <p className="text-gray-900 font-bold">{this.props.summoner.name}</p>
                     <p className="text-gray-600">{this.props.summoner.region}</p>
-                    <p className="text-gray-600">{this.props.summoner.level}</p>
+                    <p className="text-gray-600 leading-none">{this.props.summoner.level}</p>
                 </div>
             </div>
         );
