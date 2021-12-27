5b1886d99c4bb60af09f55e14036416a08bb0940
Baran Kazan <baran.kazan@hotmail.com>
2021-12-27 13:48:59 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -63,42 +63,49 @@ class App extends Component {
       button = <button onClick={this.buttonClicked} className="transition h-full w-full shadow-lg py-2 px-4 bg-blue-900 text-white font-semibold rounded-lg hover:bg-blue-800 focus:outline-none focus:ring-2 focus:ring-blue-900 focus:ring-opacity-75">Search</button>;
 
     return (
-      <div className="container mx-auto px-4">
-        <div className="flex flex-col">
-          <form className="flex flex-col sm:flex-row items-stretch w-full mt-8">
-            <div className="flex flex-col flex-grow bg-white shadow-lg rounded-lg p-3 my-2 sm:mx-2 border border-gold-800">
-              <label className="mb-1">Summoner Name:</label>
-              <input placeholder="Kinkymonkey" className="text-sm text-black placeholder-gray-500 border-2 border-gray-200 rounded-md py-2 pl-2" onChange={e => this.setState({ summonerName: e.target.value })}></input>
-            </div>
-            <div className="flex flex-col flex-none bg-white shadow-lg rounded-lg p-3 my-2 sm:mx-2 border border-gold-800">
-              <label className="mb-1">Region:</label>
-              <select className="py-2 text-sm text-black border border-gray-200 rounded-md" onChange={e => this.setState({ region: e.target.value })}>
-                <option value="BRAZIL">BR</option>
-                <option value="EUROPE_NORTH_EAST">EUNE</option>
-                <option value="EUROPE_WEST">EUW</option>
-                <option value="JAPAN">JP</option>
-                <option value="KOREA">KR</option>
-                <option value="LATIN_AMERICA_NORTH">LAN</option>
-                <option value="LATIN_AMERICA_SOUTH">LAS</option>
-                <option value="NORTH_AMERICA">NA</option>
-                <option value="OCEANIA">OCE</option>
-                <option value="RUSSIA">RU</option>
-                <option value="TURKEY">TR</option>
-              </select>
-            </div>
-            <div className="flex-none my-2 sm:mx-2">
-              {button}
-            </div>
-          </form>
-        </div>
-        {this.state.champions.length !== 0 &&
-          <>
-            {summoner}
+      <div>
+        <div className="container mx-auto px-4">
+          <div className="flex flex-col">
+            <form className="flex flex-col sm:flex-row items-stretch w-full mt-8">
+              <div className="flex flex-col flex-grow bg-white shadow-lg rounded-lg p-3 my-2 sm:mx-2 border border-gold-800">
+                <label className="mb-1">Summoner Name:</label>
+                <input placeholder="Kinkymonkey" className="text-sm text-black placeholder-gray-500 border-2 border-gray-200 rounded-md py-2 pl-2" onChange={e => this.setState({ summonerName: e.target.value })}></input>
+              </div>
+              <div className="flex flex-col flex-none bg-white shadow-lg rounded-lg p-3 my-2 sm:mx-2 border border-gold-800">
+                <label className="mb-1">Region:</label>
+                <select className="py-2 text-sm text-black border border-gray-200 rounded-md" onChange={e => this.setState({ region: e.target.value })}>
+                  <option value="BRAZIL">BR</option>
+                  <option value="EUROPE_NORTH_EAST">EUNE</option>
+                  <option value="EUROPE_WEST">EUW</option>
+                  <option value="JAPAN">JP</option>
+                  <option value="KOREA">KR</option>
+                  <option value="LATIN_AMERICA_NORTH">LAN</option>
+                  <option value="LATIN_AMERICA_SOUTH">LAS</option>
+                  <option value="NORTH_AMERICA">NA</option>
+                  <option value="OCEANIA">OCE</option>
+                  <option value="RUSSIA">RU</option>
+                  <option value="TURKEY">TR</option>
+                </select>
+              </div>
+              <div className="flex-none my-2 sm:mx-2">
+                {button}
+              </div>
+            </form>
+          </div>
+          {this.state.champions.length !== 0 &&
+            <>
+              {summoner}
               <div className="mt-3 grid p-3 bg-gray-100 border-2 border-gold-800 rounded-3xl lg:grid-cols-6 md:grid-cols-5 grid-cols-3 gap-4">
                 {champions}
-            </div>
-          </>
-        }
+              </div>
+            </>
+          }
+        </div>
+        <footer class="absolute bottom-0 w-full bg-white">
+          <div class="py-16 flex flex-col justify-center items-center">
+            <p class="mt-6 text-xs lg:text-sm text-gray-500">Developed by Baran Kazan</p>
+          </div>
+        </footer>
       </div>
     );
   }
