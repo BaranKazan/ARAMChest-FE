8ee99cd1d5c0b81f54d4c34cb8d706c27c52efa0
Baran Kazan <Baran.kazan@hotmail.com>
2022-05-01 20:37:38 +0200

    Added riot.txt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import React, { Component } from 'react';
 import Home from "./components/Home";
+import { Routes, Route } from "react-router-dom";
+import Riot from "./components/Riot";
+
 
 class App extends Component {
 
   render() {
     return (
-      <Home/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+            <Route path="/riot.txt" element={<Riot />} />
+        </Routes>
     );
   }
 }
---
+++ src/components/Riot.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+
+class Riot extends Component {
+
+    render() {
+        return (
+            <p>fc781c6f-ef81-4918-a6b0-08a7bead998d</p>
+        );
+    }
+}
+
+export default Riot;
