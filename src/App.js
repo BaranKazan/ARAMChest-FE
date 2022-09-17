9a70dab41b2fe2cbc9763285b3c8cbc29fd2a3b9
Baran Kazan <Baran.kazan@hotmail.com>
2022-09-17 18:04:31 +0200

    Deleted the txt page

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import React, { Component } from 'react';
 import Home from "./components/Home";
 import { Routes, Route } from "react-router-dom";
-import Riot from "./components/Riot";
 
 
 class App extends Component {
@@ -11,7 +10,6 @@ class App extends Component {
     return (
         <Routes>
           <Route path="/" element={<Home />} />
-            <Route path="/riot.txt" element={<Riot />} />
         </Routes>
     );
   }
